e253e7c5283676ebe4c7e0fb50642df8b7637e47
Martins100-Tmd <olaitanfabayo@gmail.com>
2024-11-27 14:32:40 +0100

    He's backkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk

--- Page 2/Index.js
+++ Page 2/Index.js
@@ -5,9 +5,35 @@ window.onload = () => {
    element.scrollTo(element.scrollWidth, 0);
 };
 
-export function SAP() {
-   gsap.from('.home', { x: -300, duration: 2 }, { x: 300, duration: 0.9 });
-   gsap.from('.collect', { x: 300, duration: 2 }, { x: -300, duration: 0.9 });
-   gsap.from('.pet', { x: -200, duration: 2 }, { x: 200, duration: 0.9 });
-   gsap.from('.contact', { x: -100, duration: 2 }, { x: 100, duration: 0.9 });
+gsap.from('.bone', { x: -250, duration: 1.2 });
+gsap.from('.paw', { scale: 0.5, duration: 1 });
+gsap.from('.best', { x: 50, duration: 1.5 });
+gsap.from('.dog', { y: -100, rotate: 270, duration: 2 });
+
+function SAP() {
+   gsap.from('.home', { x: 250, duration: 1.8 });
+   gsap.from('.collect', { x: 100, duration: 1.4 });
+   gsap.from('.pet', { x: 150, duration: 1 });
+   gsap.from('.contact', { x: 180, duration: 0.8 });
 }
+
+window.MT = () => {
+   const ele = document.querySelector('.sidebar');
+   if (ele.classList.contains('hidden')) {
+      ele.classList.remove('hidden');
+      ele.classList.add('flex');
+      SAP();
+   } else {
+      ele.classList.remove('flex');
+      ele.classList.add('hidden');
+   }
+};
+
+window.cycle = (element, btn) => {
+   const scrollContainer = document.querySelector(`.${element}`);
+   gsap.to(scrollContainer, {
+      scrollLeft: scrollContainer.scrollLeft + 100, // Scroll 300px to the right
+      duration: 1,
+      ease: 'power2.out',
+   });
+};
--- Page 2/webpack.config.js
+++ Page 2/webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
    output: {
       path: path.join(__dirname, 'dist'),
       filename: 'output.js',
+      iife: true,
    },
    mode: 'development',
 };
